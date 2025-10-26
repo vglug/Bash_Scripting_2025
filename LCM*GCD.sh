0300d2b29ef95458e611f780881253908c624282
Priyanka123-raj <p23413976@gmail.com>
2025-10-26 12:52:23 +0530

    Create LCM*GCD.sh
    To find the lcm of two numbers in bash code.

---
+++ LCM*GCD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Read two numbers
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+
+# Function to find GCD using Euclidean algorithm
+gcd() {
+    local x=$1
+    local y=$2
+    while [ $y -ne 0 ]; do
+        local temp=$y
+        y=$((x % y))
+        x=$temp
+    done
+    echo $x
+}
+
+# Calculate GCD
+g=$(gcd $a $b)
+
+# Calculate LCM
+lcm=$(( (a * b) / g ))
+
+echo "LCM of $a and $b is: $lcm"
