470d9090d0eb7363c63a7bcf853ff960deb1d9bd
Priyanka <oprpriyanka2024@gmail.com>
2025-10-26 11:50:10 +0530

    Update divisible_3_5.sh

--- divisible_3_5.sh
+++ divisible_3_5.sh
@@ -4,9 +4,9 @@
 read -p "Enter a number: " num
 
 # Check divisibility by both 3 and 5
-if (( num % 3 == 0 && num % 5 == 0 )); then
-    echo "Divisible"
+if (( num % 3 == 0 && num % 5 == 0 )); then 
+    echo "Divisible" # If the number is divisible by 3 and 5 print this
 else
-    echo "Not Divisible"
+    echo "Not Divisible" # If the number not divisible by 3 and 5 print this 
 fi
 
