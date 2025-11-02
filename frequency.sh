1397eae186a4c4357118c74a09e7bd8f9b2a10be
SHYAM V <shayamshyamv@gmail.com>
2025-11-02 19:15:44 +0530

    Implement frequency count for array elements
    Added functionality to read an array and count element frequencies using an associative array.

--- frequency.sh
+++ frequency.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+# Read array elements
 read -p "Enter array elements (space-separated): " -a arr
+
+# Count frequency using associative array
 declare -A freq
+
 for i in "${arr[@]}"
 do
     ((freq[$i]++))
 done
+
+# Display frequency of each element
 echo "Element frequencies:"
 for i in "${!freq[@]}"
 do
